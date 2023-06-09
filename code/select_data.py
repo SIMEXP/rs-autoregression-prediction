caa9e8d503879db671021f40a4b0f7de28070d72
Hao-Ting Wang <htwangtw@gmail.com>
2023-06-09 13:18:55 -0400

    start to work on tr resampling

--- code/select_data.py
+++ code/select_data.py
@@ -1,20 +1,36 @@
-"""Select data based on QC results. Save as HDF5."""
+"""Select data based on abide QC results. Save as HDF5."""
 from pathlib import Path
+
 import h5py
+import numpy as np
 import pandas as pd
+from scipy.interpolate import interp1d
+
+field_mappers = {
+    "abide1": {"site_name": "site_name", "SUB_ID": "SUB_ID"},
+    "abide2": {"site_name": "SITE_ID", "SUB_ID": "SUB_ID"},
+}
+
+tr_mapper = {
+    "abide1": {"CMU_a": 2.5},
+    "abide2": {"BNI_1": 2.5},
+}
+
+TARGET_TR = 2.5
 
 
 def _traverse_datasets(hdf_file):
     """Load nested hdf5 files.
     https://stackoverflow.com/questions/51548551/reading-nested-h5-group-into-numpy-array
     """
-    def h5py_dataset_iterator(g, prefix=''):
+
+    def h5py_dataset_iterator(g, prefix=""):
         for key in g.keys():
             item = g[key]
-            path = f'{prefix}/{key}'
-            if isinstance(item, h5py.Dataset): # test for dataset
+            path = f"{prefix}/{key}"
+            if isinstance(item, h5py.Dataset):  # test for dataset
                 yield (path, item)
-            elif isinstance(item, h5py.Group): # test for group (go down)
+            elif isinstance(item, h5py.Group):  # test for group (go down)
                 yield from h5py_dataset_iterator(item, path)
 
     for path, _ in h5py_dataset_iterator(hdf_file):
@@ -48,26 +64,94 @@ def _fetch_h5_group(f, subject, session):
         return f[f"{subject}"]
 
 
-if __name__ == "__main__":
-    qc_abide1 = pd.read_csv("inputs/connectomes/sourcedata/abide1_connectomes/ABIDE1_Pheno_PSM_matched.tsv", sep='\t', index_col=0)
-
-    path_abide1 = Path("inputs/connectomes/sourcedata/abide1_connectomes/").glob("*/atlas-MIST_desc-simple+gsr.h5")
-
-
-    with h5py.File("inputs/connectomes/abide1.h5", "a") as h5file:
-        for p in path_abide1:
+def concat_h5(path_h5s, output_h5):
+    """Concatenate connectome h5 files."""
+    # with tb.File('table_copy_2.h5',mode='w') as h5fw:
+    #     for h5name in glob.glob('file*.h5'):
+    #         h5fr = tb.File(h5name,mode='r')
+    #         print (h5fr.root._v_children)
+    #     h5fr.root._f_copy_children(h5fw.root)
+    with h5py.File(output_h5, "a") as h5file:
+        for p in path_h5s:
+            print(p)
             site_name = p.parent.name
-            site_filter = qc_abide1["site_name"] == site_name
-            subjects = qc_abide1.loc[site_filter, "SUB_ID"].values
-            if len(subjects) > 0:
-                site = h5file.create_group(site_name)
-                with h5py.File(p, "r") as f:
-                    for dset in _traverse_datasets(f):
-                        if "sub" in dset:
-                            subject, session, dataset_name = _parse_path(dset)
-                            current_sub = int(subject.split("sub-00")[-1])
-                            if current_sub in subjects:
-                                g = _fetch_h5_group(f, subject, session)
-                                data = f[dset][:]
-                                # save data
-                                g.create_dataset(dataset_name, data=data)
+            site = h5file.create_group(site_name)
+            with h5py.File(p, "r") as f:
+                for dset in _traverse_datasets(f):
+                    subject, session, dataset_name = _parse_path(dset)
+                    g = _fetch_h5_group(site, subject, session)
+                    data = f[dset][:]
+                    # save data
+                    if "rest" in dataset_name:
+                        g.create_dataset(dataset_name, data=data)
+                    else:
+                        site.create_dataset(dataset_name, data=data)
+    return output_h5
+
+
+def _resample_tr(data, original_tr):
+    if TARGET_TR == original_tr:
+        return data
+    time_stamp_original = np.arange(0, data.shape[0]) * original_tr
+    scan_length = data.shape[0] * data.shape[0]
+    n_resampled_timepoints = int(scan_length / TARGET_TR)
+    time_stamp_new = np.arange(0, n_resampled_timepoints) * TARGET_TR
+    f = interp1d(time_stamp_original, data)
+    return f(time_stamp_new)
+
+
+def _process_data(site_name, subjects, f, dset):
+    if "sub" not in dset or "connectome" in dset:
+        del f[dset]
+        print(f"remove {dset}")
+    subject, _, _ = _parse_path(dset)
+    print(subject)
+    current_sub = subject.split("sub-")[-1]
+    if current_sub != "atlas-MIST":
+        current_sub = int(subject.split("sub-")[-1])
+    if current_sub == "atlas-MIST":
+        pass
+    elif current_sub not in subjects:
+        del f[dset]
+        print(f"remove {dset}")
+    else:
+        print(f"resample {dset}")
+        data = f[dset][:]
+        # resample the time series
+        f[dset][:] = _resample_tr(data, tr_mapper[site_name])
+
+
+def main():
+    for key in field_mappers:
+        path_connectomes = Path(
+            f"inputs/connectomes/sourcedata/{key}_connectomes/"
+        ).glob("*/atlas-MIST_desc-simple+gsr.h5")
+        path_concat = f"inputs/connectomes/{key}.h5"
+        if not Path(path_concat).exists():
+            path_concat = concat_h5(path_connectomes, path_concat)
+        print("concatenated across site")
+
+        # select subject based on each site, keep time series only and resample
+        qc = pd.read_csv(
+            f"inputs/connectomes/sourcedata/{key}_connectomes/{key.upper()}_Pheno_PSM_matched.tsv",
+            sep="\t",
+        )
+        qc["site_name"] = qc[field_mappers[key]["site_name"]].replace(
+            {"ABIDEII-": ""}, regex=True
+        )
+        all_sites = qc["site_name"].unique()
+
+        for site_name in all_sites:
+            print(site_name)
+            site_filter = qc["site_name"] == site_name
+            subjects = qc.loc[site_filter, field_mappers[key]["SUB_ID"]].values
+            if len(subjects) == 0:
+                continue
+
+            with h5py.File(path_concat, "a") as f:
+                for dset in _traverse_datasets(f):
+                    _process_data(site_name, subjects, f, dset)
+
+
+if __name__ == "__main__":
+    main()
