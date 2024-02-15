354addd2578d3229ac8b6b1ad4deeaf9c979805e
Hao-Ting Wang <htwangtw@gmail.com>
2024-02-14 20:54:02 -0500

    FIX output path

--- src/predict.py
+++ src/predict.py
@@ -64,6 +64,7 @@ def main(params: DictConfig) -> None:
 
     # parse parameters
     output_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
+    output_dir = Path(output_dir)
     convlayers_path = Path(params["convlayers_path"])
     feature_t1_file = (
         convlayers_path.parent / f"feature_horizon-{params['horizon']}.h5"
