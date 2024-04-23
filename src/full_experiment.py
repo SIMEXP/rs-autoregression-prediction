FIX create a complete holdout set for scaling experiment

src/full_experiment.py
@@ -29,7 +29,7 @@ from sklearn.linear_model import (
     RidgeClassifier,
 )
 from sklearn.metrics import r2_score
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import ShuffleSplit, StratifiedKFold
 from sklearn.neural_network import MLPClassifier, MLPRegressor
 from sklearn.svm import LinearSVC, LinearSVR
 
@@ -123,18 +123,31 @@ def main(params: DictConfig) -> None:
     log.info(f"Working on {device}.")
 
     # load data path
+    with open(params["data_split"], "r") as f:
+        data_split = json.load(f)
+    data_reference = {}
+    data_reference["test"] = data_split["test"]
+    training_candidate = data_split["train"] + data_split["val"]
     n_sample = params["data"]["split"]["n_sample"]
-    data_reference = instantiate(params["data"]["split"])
-
-    with open(Path(output_dir) / "train_test_split.json", "w") as f:
-        json.dump(data_reference, f, indent=2)
-    if n_sample == -1:
-        n_sample = (
-            len(data_reference["train"])
-            + len(data_reference["val"])
-            + len(data_reference["test"])
+    rs = ShuffleSplit(
+        n_splits=1, test_size=0.25, random_state=params["random_state"]
+    )
+    if n_sample != -1:
+        proportion = n_sample / len(training_candidate)
+        sample_select = ShuffleSplit(
+            n_splits=1,
+            train_size=proportion,
+            random_state=params["random_state"],
         )
-    log.info(f"Experiment on {n_sample} subjects. ")
+        sample_index = next(sample_select.split(training_candidate))
+        pretraining_set = [training_candidate[i] for i in sample_index]
+    else:
+        pretraining_set = training_candidate.copy()
+    train_index, val_index = next(rs.split(pretraining_set))
+    data_reference["train"] = [pretraining_set[i] for i in train_index]
+    data_reference["val"] = [pretraining_set[i] for i in val_index]
+
+    log.info(f"Pretrain model on {len(pretraining_set)} subjects.")
 
     tng_data_h5 = data_dir / "data_train.h5"
     val_data_h5 = data_dir / "data_val.h5"
@@ -207,7 +220,6 @@ def main(params: DictConfig) -> None:
 
     log.info("Start extracting features.")
     model = model.eval()
-    subj_list = data_reference["test"]
     horizon = 1
     output_horizon_path = Path(output_dir) / f"feature_horizon-{horizon}.h5"
     with h5py.File(output_horizon_path, "a") as f:
@@ -216,7 +228,7 @@ def main(params: DictConfig) -> None:
         f.attrs["horizon"] = horizon
 
     log.info(f"Predicting t+{horizon} of each subject")
-    for h5_dset_path in subj_list:
+    for h5_dset_path in data_reference["test"]:
         # get the prediction of t+1
         r2, Z, Y = predict_horizon(
             model=model,
@@ -251,7 +263,7 @@ def main(params: DictConfig) -> None:
     thres = params["model"]["edge_index_thres"] if compute_edge_index else None
     if isinstance(model, torch.nn.Module):
         model.to(torch.device(device)).eval()
-    for h5_dset_path in subj_list:
+    for h5_dset_path in data_reference["test"]:
         convlayers = extract_convlayers(
             data_file=params["data"]["data_file"],
             h5_dset_path=h5_dset_path,
@@ -299,7 +311,7 @@ def main(params: DictConfig) -> None:
             baseline_details[key]["data_file"] = output_conv_path
         elif "connectome" in key:
             baseline_details[key]["data_file"] = params["data"]["data_file"]
-            baseline_details[key]["data_file_pattern"] = subj_list
+            baseline_details[key]["data_file_pattern"] = data_reference["test"]
         else:
             pass
     log.info("Predicting sex.")

src/utils/plot_scaling.py
@@ -22,174 +22,168 @@ def peek(iterable):
     return itertools.chain([first], iterable)
 
 
-PATH_PATTERN_SUCCESS_JOB = {
-    'FK="8,3,8,3,8,3";M="1"': "multiruns/*/++experiment.scaling.n_sample=*,++experiment.scaling.segment=?,++random_state=*/r2_test.npy",
-    'FK="32,6,32,6,32,6,16,6,16,6";M="32,16,8,1"': 'multiruns/*/++experiment.scaling.n_sample=*,++experiment.scaling.segment=1,++model.FK="32,6,32,6,32,6,16,6,16,6",++model.M="32,16,8,1",++model.dropout=0.1,++random_state=*/r2_test.npy',
-    'FK="128,32,128,32,128,32,128,32,128,32,64,16,64,16";M="128,64,32,16,8,1"': 'multiruns/*/++experiment.scaling.n_sample=*,++experiment.scaling.segment=1,++model.FK="128,32,128,32,128,32,128,32,128,32,64,16,64,16",++model.M="128,64,32,16,8,1",++model.dropout=0.1,++random_state=*/r2_test.npy',
-}
-
-
 def main():
-    stats = {}
-    for name, pattern in PATH_PATTERN_SUCCESS_JOB.items():
-        path_success_job = Path("outputs/scaling").glob(pattern)
-        path_success_job = peek(path_success_job)
+    path_success_job = Path(
+        "outputs/autoreg/full_experiment/ccn2024_scaling"
+    ).glob("*/*/simple_classifiers_sex.tsv")
+    path_success_job = peek(path_success_job)
 
-        # if no path found, skip to next item
-        if path_success_job is None:
-            continue
+    scaling_stats = pd.DataFrame()
+    for p in path_success_job:
+        # parse the path and get number of subjects
+        n_sample = int(p.parts[-2].split("n-")[-1].split("_")[0])
+        if n_sample == -1:
+            n_sample = 25992
+        # get random seed
+        random_seed = int(p.parts[-2].split("seed-")[-1].split("_")[0])
+        # load r2_val.npy get mean r2
+        mean_r2_val = np.load(p.parent / "mean_r2_val.npy").tolist()
+        mean_r2_tng = np.load(p.parent / "mean_r2_tng.npy").tolist()
+        # get runtime from log file text
+        log_file = p.parent / "full_experiment.log"
+        with open(log_file, "r") as f:
+            log_text = f.read()
+            starttime = re.search(
+                r"\[([\d\-\s:,]*)\].*Process ID", log_text
+            ).group(1)
+            endtime = re.search(
+                r"\[([\d\-\s:,]*)\].*model trained", log_text
+            ).group(1)
+            starttime = pd.to_datetime(starttime)
+            endtime = pd.to_datetime(endtime)
+            runtime = endtime - starttime
+            # convert to log scale
+            runtime = runtime.total_seconds() / 60
+            runtime_log = np.log10(runtime)
 
-        scaling_stats = pd.DataFrame()
-        for p in path_success_job:
-            # parse the path and get number of subjects
-            n_sample = int(p.parts[-2].split("n_sample=")[-1].split(",")[0])
-            if n_sample == -1:
-                n_sample = 25992
-            # get random seed
-            random_seed = int(
-                p.parts[-2].split("random_state=")[-1].split(",")[0]
-            )
-            # load r2_val.npy get mean r2
-            r2_test = np.load(p)
-            mean_r2_test = r2_test.mean()
-            r2_val = np.load(p.parent / "r2_val.npy")
-            mean_r2_val = r2_val.mean()
-            r2_tng = np.load(p.parent / "r2_tng.npy")
-            mean_r2_tng = r2_tng.mean()
-            # get runtime from log file text
-            log_file = p.parent / "scaling.log"
-            with open(log_file, "r") as f:
-                log_text = f.read()
-                starttime = re.search(
-                    r"\[([\d\-\s:,]*)\].*Process ID", log_text
-                ).group(1)
-                endtime = re.search(
-                    r"\[([\d\-\s:,]*)\].*completed", log_text
-                ).group(1)
-                starttime = pd.to_datetime(starttime)
-                endtime = pd.to_datetime(endtime)
-                runtime = endtime - starttime
-                # convert to log scale
-                runtime = runtime.total_seconds() / 60
-                runtime_log = np.log10(runtime)
-
-            df = pd.DataFrame(
-                [
-                    n_sample,
-                    random_seed,
-                    mean_r2_test,
-                    mean_r2_val,
-                    mean_r2_tng,
-                    runtime,
-                    runtime_log,
-                ],
-                index=[
-                    "n_sample",
-                    "random_seed",
-                    "mean_r2_test",
-                    "mean_r2_val",
-                    "mean_r2_tng",
-                    "runtime",
-                    "runtime_log",
-                ],
-            ).T
-            scaling_stats = pd.concat([scaling_stats, df], axis=0)
-
-        # sort by n_sample
-        scaling_stats = scaling_stats.sort_values(by="n_sample")
-        # for each n_sample, sort by random seed
-        scaling_stats = scaling_stats.groupby("n_sample").apply(
-            lambda x: x.sort_values(by="random_seed")
+        # load connectome accuracy
+        prediction = pd.read_csv(
+            p.parent / "simple_classifiers_sex.tsv", sep="\t", index_col=0
         )
-        scaling_stats = scaling_stats.reset_index(drop=True)
+        prediction = prediction.loc[
+            prediction["classifier"] == "SVM", ["feature", "score"]
+        ]
+        prediction = prediction.set_index("feature")
+        prediction = prediction.T.reset_index(drop=True)
 
-        scaling_stats.to_csv(f"outputs/scaling/model-{name}_scaling_data.csv")
-        stats[name] = scaling_stats
-        # alternative data to show missing experiment
-        # random seed as column and runtime as value
-        scaling_overview = scaling_stats.pivot(
-            index="n_sample", columns="random_seed", values="mean_r2_val"
-        )
+        df = pd.DataFrame(
+            [
+                n_sample,
+                random_seed,
+                mean_r2_val,
+                mean_r2_tng,
+                runtime,
+                runtime_log,
+            ],
+            index=[
+                "n_sample",
+                "random_seed",
+                "mean_r2_val",
+                "mean_r2_tng",
+                "runtime",
+                "runtime_log",
+            ],
+        ).T
+        df = pd.concat([df, prediction], axis=1)
+        scaling_stats = pd.concat([scaling_stats, df], axis=0)
 
-        # give a summary of the random seed and n_sample pair
-        # with no runtime. this is because the experiment failed
-        incomplete_n_sample = scaling_overview.isna().sum(axis=1)
-        incomplete_n_sample = incomplete_n_sample[incomplete_n_sample > 0]
-        # make sure all possible n_sample are included
-        for n_sample in scaling_overview.index:
-            if n_sample not in incomplete_n_sample.index:
-                incomplete_n_sample[n_sample] = 0
-        incomplete_n_sample = incomplete_n_sample.sort_index()
-        missing_experiment = {}
-        for n_sample in incomplete_n_sample.index:
-            missing_experiment[n_sample] = scaling_overview.columns[
-                scaling_overview.loc[n_sample].isna()
-            ].tolist()
-        # save to json
-        with open(
-            f"outputs/scaling/model-{name}_scaling_missing_experiment.json",
-            "w",
-        ) as f:
-            json.dump(missing_experiment, f, indent=2)
+    # sort by n_sample
+    scaling_stats = scaling_stats.sort_values(by="n_sample")
+    # for each n_sample, sort by random seed
+    scaling_stats = scaling_stats.groupby("n_sample").apply(
+        lambda x: x.sort_values(by="random_seed")
+    )
+    scaling_stats = scaling_stats.reset_index(drop=True)
 
-    plt.figure(figsize=(7, 5))
-    for key, df in stats.items():
-        # plot
-        sns.lineplot(
-            data=df,
-            x="n_sample",
-            y="mean_r2_tng",
-            marker="o",
-            label=key.replace(";", "\n"),
-        )
-    plt.ylabel("R^2")
+    scaling_stats.to_csv("outputs/ccn2024/scaling_data.csv")
+    # stats[name] = scaling_stats
+    # alternative data to show missing experiment
+    # random seed as column and runtime as value
+    scaling_overview = scaling_stats.pivot(
+        index="n_sample", columns="random_seed", values="mean_r2_val"
+    )
+
+    # give a summary of the random seed and n_sample pair
+    # with no runtime. this is because the experiment failed
+    incomplete_n_sample = scaling_overview.isna().sum(axis=1)
+    incomplete_n_sample = incomplete_n_sample[incomplete_n_sample > 0]
+    # make sure all possible n_sample are included
+    for n_sample in scaling_overview.index:
+        if n_sample not in incomplete_n_sample.index:
+            incomplete_n_sample[n_sample] = 0
+    incomplete_n_sample = incomplete_n_sample.sort_index()
+    missing_experiment = {}
+    for n_sample in incomplete_n_sample.index:
+        missing_experiment[n_sample] = scaling_overview.columns[
+            scaling_overview.loc[n_sample].isna()
+        ].tolist()
+    # save to json
+    with open(
+        "outputs/ccn2024/scaling_missing_experiment.json",
+        "w",
+    ) as f:
+        json.dump(missing_experiment, f, indent=2)
+
+    plt.figure(figsize=(7, 4.5))
+    # plot
+    sns.lineplot(
+        data=scaling_stats,
+        x="n_sample",
+        y="mean_r2_tng",
+        marker="o",
+        label="Traing set",
+    )
+    sns.lineplot(
+        data=scaling_stats,
+        x="n_sample",
+        y="mean_r2_val",
+        marker="o",
+        label="Validation set",
+    )
+    plt.ylim(0.12, 0.19)
+    plt.xlabel("Number of subject in the scaling experiment")
+    plt.ylabel("R-squared")
     plt.legend()
-    plt.title("R^2 of the Training set")
-    plt.savefig("outputs/scaling/scaling_r2_tng_plot.png")
+    plt.title("R-squared of t+1 prediction")
+    plt.savefig("outputs/ccn2024/scaling_r2_tng_plot.png")
     plt.close()
-    plt.figure(figsize=(7, 5))
-    for key, df in stats.items():
-        # plot
-        sns.lineplot(
-            data=df,
-            x="n_sample",
-            y="mean_r2_val",
-            marker="o",
-            label=key.replace(";", "\n"),
-        )
-    plt.ylabel("R^2")
-    plt.legend()
-    plt.title("R^2 of the Validation set")
-    plt.savefig("outputs/scaling/scaling_r2_val_plot.png")
-    plt.close()
-    plt.figure(figsize=(7, 5))
-    for key, df in stats.items():
-        # plot
-        sns.lineplot(
-            data=df,
-            x="n_sample",
-            y="mean_r2_test",
-            marker="o",
-            label=key.replace(";", "\n"),
-        )
-    plt.ylabel("R^2")
-    plt.legend()
-    plt.title("R^2 of the Test set")
-    plt.savefig("outputs/scaling/scaling_r2_test_plot.png")
-    plt.close()
-    plt.figure(figsize=(7, 5))
-    for key, df in stats.items():
-        sns.lineplot(
-            data=df,
-            x="n_sample",
-            y="runtime_log",
-            marker="o",
-            label=key.replace(";", "\n"),
-        )
+
+    plt.figure(figsize=(7, 4.5))
+    sns.lineplot(
+        data=scaling_stats,
+        x="n_sample",
+        y="runtime_log",
+        marker="o",
+    )
+    plt.xlabel("Number of subject in the scaling experiment")
     plt.ylabel("log10(runtime) (minutes)")
     plt.title("Runtime of training a group model")
+    plt.savefig("outputs/ccn2024/scaling_runtime_plot.png")
+    plt.close()
+
+    plt.figure(figsize=(7, 4.5))
+    # plot
+    features = prediction.columns.tolist()
+    for y, label in zip(
+        features,
+        [
+            "connectomes",
+            "average pooling",
+            "standard deviation pooling",
+            "max pooling",
+            "1D convolution",
+            "average R-squared",
+            "R-squared map",
+        ],
+    ):
+        sns.lineplot(
+            data=scaling_stats, x="n_sample", y=y, marker="o", label=label
+        )
+    plt.xlabel("Number of subject in the scaling experiment")
+    plt.ylabel("Accuracy")
     plt.legend()
-    plt.savefig("outputs/scaling/scaling_runtime_plot.png")
+    plt.title("Sex prediction accuracy with SVM")
+    plt.savefig("outputs/ccn2024/scaling_connectome.png")
     plt.close()
 
 