87e1d71fe0ce9aa6fde0f5e3e796f1b8645ffbc0
Hao-Ting Wang <htwangtw@gmail.com>
2024-07-17 16:04:44 -0400

    save demographic info summary

--- src/create_holdout_sample.py
+++ src/create_holdout_sample.py
@@ -45,6 +45,7 @@ with open(output_downstream_sample, "w") as f:
     json.dump(sample, f, indent=2)
 
 # plot the distribution of confounds of downstreams balanced samples
+demographics = {}
 for d in sample["test_downstreams"].keys():
     d_subjects = sample["test_downstreams"][d]
     df = data.loc[d_subjects, :]
@@ -60,6 +61,39 @@ for d in sample["test_downstreams"].keys():
     for ax, c in zip(axes, class_balance_confounds):
         sns.histplot(x=c, data=df, hue=d, kde=True, ax=ax)
     fig.savefig(output_reports + f"/{d}.png")
+    demographics[d] = {
+        "patient": {
+            "condition": d,
+            "total": df[df[d] == 1].shape[0],
+            "n_female": df[df[d] == 1].shape[0] - df[df[d] == 1]["sex"].sum(),
+            "age_mean": df[df[d] == 1]["age"].mean(),
+            "age_sd": df[df[d] == 1]["age"].std(),
+            "mean_fd_mean": df[df[d] == 1]["mean_fd_raw"].mean(),
+            "mean_fd_sd": df[df[d] == 1]["mean_fd_raw"].std(),
+            "proportion_kept_mean": df[df[d] == 1]["proportion_kept"].mean(),
+            "proportion_kept_sd": df[df[d] == 1]["proportion_kept"].std(),
+        },
+        "control": {
+            "condition": d,
+            "total": df[df[d] == 0].shape[0],
+            "n_female": df[df[d] == 0].shape[0] - df[df[d] == 0]["sex"].sum(),
+            "age_mean": df[df[d] == 0]["age"].mean(),
+            "age_sd": df[df[d] == 0]["age"].std(),
+            "mean_fd_mean": df[df[d] == 0]["mean_fd_raw"].mean(),
+            "mean_fd_sd": df[df[d] == 0]["mean_fd_raw"].std(),
+            "proportion_kept_mean": df[df[d] == 0]["proportion_kept"].mean(),
+            "proportion_kept_sd": df[df[d] == 0]["proportion_kept"].std(),
+        },
+    }
+
+demographics_summary = pd.DataFrame()
+for d in demographics.keys():
+    df = pd.DataFrame.from_dict(demographics[d], orient="index")
+    df.set_index([df.index, "condition"], inplace=True)
+    demographics_summary = pd.concat([demographics_summary, df])
+demographics_summary.round(decimals=2).to_csv(
+    output_reports + "/demographics_summary.tsv", sep="\t"
+)
 
 for key in sample.keys():
     if key == "test_downstreams":
